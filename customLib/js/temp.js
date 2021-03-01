6e9af3c94e639ecdb5ba46f61d672ec2ba788bf3
CodeDude404 <68038925+CodeDude404@users.noreply.github.com>
2021-03-01 15:12:42 -0800

    eix undefined error

--- customLib/js/temp.js
+++ customLib/js/temp.js
@@ -3,14 +3,14 @@ var inputCelsius = document.getElementById("inputCelsius");
 var inputKelvin = document.getElementById("inputKelvin");
 
 function ConvertF(input) {
-	inputCelsius.value = ((valNum - 32) / 1.8);
-	inputKelvin.value = (((valNum - 32) / 1.8) + 273.15);
+	inputCelsius.value = ((input - 32) / 1.8);
+	inputKelvin.value = (((input - 32) / 1.8) + 273.15);
 }
 function ConvertC(input) {
-	inputFahrenheit.value = ((valNum * 1.8) + 32);
-	inputKelvin.value = ((valNum) + 273.15);
+	inputFahrenheit.value = ((input * 1.8) + 32);
+	inputKelvin.value = ((input) + 273.15);
 }
 function ConvertK(input) {
-	inputFahrenheit.value = (((valNum - 273.15) * 1.8) + 32);
-	inputCelsius.value = ((valNum) - 273.15);
+	inputFahrenheit.value = (((input - 273.15) * 1.8) + 32);
+	inputCelsius.value = ((input) - 273.15);
 }
